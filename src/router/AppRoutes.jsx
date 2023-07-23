6bd6063e9ded3e1b5e182bdc837533626cea1d51
Joaquin Rodriguez <joaquinrodriguez.dev@gmail.com>
2023-07-23 06:12:29 -0300

    Install Material UI, and HomePage created

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
+import HomePagePrueba from '../pages/HomePagePrueba';
 
 function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ function AppRoutes() {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<HomePage/>}/>
+                <Route path='/prueba' element={<HomePagePrueba/>}/>
                 <Route path='*' element={<h1>Pagina no disponible</h1>}/>
             </Routes>
         </BrowserRouter>
